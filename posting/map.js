d14ff0539bef4509b3d067dc145b9be462cdfe53
hayuk1020 <122197362+hayuk1020@users.noreply.github.com>
2023-03-28 18:47:30 +0900

    지도 api 수정

--- posting/map.js
+++ posting/map.js
@@ -1,10 +1,24 @@
-var mapOptions = {
-    center: new naver.maps.LatLng(37.5666805, 126.9784147), // 서울 중심 좌표
-    zoom: 10 // 지도 확대 레벨
+var container = document.getElementById('map');
+var options = {
+	center: new kakao.maps.LatLng(36.3696881172841, 127.346645436586),
+	level: 3
 };
-var map = new naver.maps.Map('map', mapOptions); // 지도를 생성하고 id가 "map"인 div에 넣어줍니다.
 
-var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.5666805, 126.9784147),
-    map: map
-}); // 마커 생성하고 지도 위에 표시합니다.
+var map = new kakao.maps.Map(container, options);
+
+// 이전 마커 객체를 저장할 변수
+var marker;
+
+// 지도를 클릭했을 때 마커를 생성하고 이전 마커를 삭제하는 이벤트 핸들러
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+    // 이전 마커가 있으면 삭제
+    if (marker) {
+        marker.setMap(null);
+    }
+    // 클릭한 위치에 마커 생성
+    var latlng = mouseEvent.latLng;
+    marker = new kakao.maps.Marker({
+        position: latlng,
+        map: map
+    });
+});
