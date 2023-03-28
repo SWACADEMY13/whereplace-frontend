eef1785ba0e873de1484bedf66bf22645d03edd1
hayuk1020 <122197362+hayuk1020@users.noreply.github.com>
2023-03-28 18:46:48 +0900

    지도 api 수정

--- location_search/location.js
+++ location_search/location.js
@@ -1,15 +1,29 @@
-var mapDiv = document.getElementById('map');
-
-var map = new naver.maps.Map(mapDiv, mapOptions);
-
-var marker = new naver.maps.Marker({
-  position: new naver.maps.LatLng(37.5112, 127.0981),
-  map: map
-});
-
-var mapOptions = {
-  center: new naver.maps.LatLng(37.3595704, 127.105399),
-  zoom: 16
+var container = document.getElementById('map');
+var options = {
+  center: new kakao.maps.LatLng(36.37030393452603, 127.34824875696489),
+  level: 6
 };
 
-var map = new naver.maps.Map('map', mapOptions); 
+var map = new kakao.maps.Map(container, options);
+
+// 마커를 생성하는 함수
+function createMarker(latlng) {
+  var marker = new kakao.maps.Marker({
+    position: latlng,
+    map: map
+  });
+
+  // 마커 클릭 시 이벤트 추가
+  kakao.maps.event.addListener(marker, 'click', function() {
+    // TODO: 게시판 글 목록을 표시하는 UI를 만들어서 표시하기
+    // 일단 alert로 대체
+    alert('게시판 글 목록');
+  });
+}
+
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+  var latlng = mouseEvent.latLng;   
+  
+  // 마커를 생성하고, 생성한 마커에 클릭 이벤트를 추가
+  createMarker(latlng);
+});
